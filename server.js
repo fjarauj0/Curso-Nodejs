d51d6b6dec951988a681cfa617cab218ed9087ae
Francisco Araujo <fjaraujo.dev@gmail.com>
2020-12-09 19:42:40 -0600

    Mensaje con GET y POST

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const router = express.Router();
+
+var app = express();
+app.use(router);
+
+router.get("/", function (req, res) {
+  res.send("Hola desde get");
+});
+
+router.post("/", function (req, res) {
+  res.send("Hola desde post");
+});
+
+// app.use("/", function (req, res) {
+//   res.send("Hola");
+// });
+
+app.listen(3000);
+console.log("La aplicación esta escuchando en http://localhost:3000");
