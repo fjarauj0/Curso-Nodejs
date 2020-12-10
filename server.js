e7a6229c9ef7d4d96fb26f6a9d8826de671f05f8
Francisco Araujo <fjaraujo.dev@gmail.com>
2020-12-09 19:55:23 -0600

    Ejemplo con body y query

--- server.js
+++ server.js
@@ -1,15 +1,20 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const router = express.Router();
 
 var app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(router);
 
-router.get("/", function (req, res) {
+router.get("/message", function (req, res) {
   res.send("Hola desde get");
 });
 
-router.post("/", function (req, res) {
-  res.send("Hola desde post");
+router.post("/message", function (req, res) {
+  console.log(req.body);
+  console.log(req.query);
+  res.send("Mensaje " + req.body.text + " añadido correctamente");
 });
 
 // app.use("/", function (req, res) {
